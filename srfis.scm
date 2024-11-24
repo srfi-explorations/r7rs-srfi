6973d2a9901f1d760d713d66ee4000999cc38f07
retropikzel <retropikzel@iki.fi>
2024-11-24 08:50:47 +0200

    Fixing Jenkins tests

--- srfis.scm
+++ srfis.scm
@@ -1,10 +1,10 @@
 ; Testing related SRFI's must be first
 ; If SRFI is dependent on another SRFI it should be after it
 (define srfis
-  '(((number . 64)) ; All depend on this for tests
-    ((number . 8))
+  '(((number . 8))
     ((number . 26))
     ((number . 28))
     ((number . 1)) ; Depends on 8
+    ((number . 64)) ; Depends on 1, 8, 26, 28
     ((number . 71))
     ))
