783e33f210ef9c02fb69de5ace8caa70041913d5
retropikzel <retropikzel@iki.fi>
2025-01-08 17:18:24 +0200

    Making builds green

--- jenkins_scripts/test.sh
+++ jenkins_scripts/test.sh
@@ -7,8 +7,6 @@ cmd="$2"
 lib_cmd="$3"
 srfis=$(cat srfi-numbers.txt)
 
-cat Makefile
-
 ## Build SRFI libraries in dependency order always first
 #for n in 8 1 60 13 14 26 28 38 39 48 64
 for srfi in 8 1 38 39 48 64
