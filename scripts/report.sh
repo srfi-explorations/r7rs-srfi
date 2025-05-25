325ac0f5782acff0494f00e07f44b23d42eb637c
retropikzel <retropikzel@iki.fi>
2025-05-25 13:24:01 +0300

    Add all results to report

--- scripts/report.sh
+++ scripts/report.sh
@@ -3,14 +3,29 @@
 echo "<table>"
 for srfi in $(cat tmp/srfis.txt)
 do
-    echo "<tr><th>$srfi</th><th>expected passes</th><th>expected failures</th></tr>"
+    echo "<tr><th>$srfi</th>"
+    echo "<th>expected passes</th>"
+    echo "<th>expected failures</th>"
+    echo "<th>failures</th>"
+    echo "<th>skipped tests</th>"
+    echo "</tr>"
     for logfile in $(find logs -name "*$srfi*")
     do
         name=$(basename "$logfile" | sed "s/-srfi-$srfi\.log//")
-        expected_passes=$(grep "# of expected passes" "$logfile" | sed 's/# of expected passes//' | xargs)
 
         printf "<tr><td>$name</td>"
+
+        expected_passes=$(grep "# of expected passes" "$logfile" | sed 's/# of expected passes//' | xargs)
         echo "<td>$expected_passes</td></tr>"
+
+        expected_failures=$(grep "# of expected failures" "$logfile" | sed 's/# of expected failures//' | xargs)
+        echo "<td>$expected_failures</td></tr>"
+
+        failures=$(grep "# of failures" "$logfile" | sed 's/# of failures//' | xargs)
+        echo "<td>$failures</td></tr>"
+
+        skipped=$(grep "# of skipped tests" "$logfile" | sed 's/# of skipped tests//' | xargs)
+        echo "<td>$skipped</td></tr>"
     done
 done
 echo "</table>"
