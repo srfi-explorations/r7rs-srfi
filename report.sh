096657b8ba1b2272cc2ace6153f29fe43e2ee0ea
retropikzel <retropikzel@iki.fi>
2026-02-09 17:38:53 +0200

    Add report script

---
+++ report.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+{
+    echo "<table>"
+    echo "<thead>"
+    echo "<tr><td>RNRS</td><td>SRFI</td><td>Scheme</td></tr>"
+    echo "</thead>"
+    echo "<tbody>"
+} > report.html
+
+for line in $(find . -name "*-line.html"); do
+    cat "$line" >> report.html
+done
+
+{
+    echo "</tbody>"
+    echo "</table>"
+} >> report.html
