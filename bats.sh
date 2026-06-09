195b632b97a56e252c5e2a9d88b5677a70d76d11
retropikzel <retropikzel@iki.fi>
2026-06-09 16:22:41 +0300

    Add more SRFIS to testing list

--- bats.sh
+++ bats.sh
@@ -2,8 +2,8 @@
 
 implementations="chibi chicken foment gauche kawa mosh racket \
 sagittarius skint stklos tr7 ypsilon"
-srfis="1 2 5 8 11 14 16 19 25 26 28 29 37 38 39 41 42 43 48 51 54 60 63 66 64 87 69 \
-95 111 113 115 116 128 145 180 197 227"
+srfis="1 2 5 8 11 14 16 19 25 26 28 29 31 37 38 39 41 42 43 48 51 60 63 64 66 \
+69 87 95 111 113 115 116 128 145 180 197 227"
 
 printf "" > "tests.bats"
 
@@ -13,9 +13,9 @@ for srfi in $srfis; do
             {
                 echo "# bats test_tags=${scheme}, ${srfi}"
                 echo "@test \"${scheme}_srfi-${srfi}\" {"
-                echo "  timeout 120 snow-chibi install --impls=${scheme} --always-yes srfi.64"
+                echo "  timeout 600 snow-chibi install --impls=${scheme} --always-yes srfi.64"
                 echo "  timeout 600 make SRFI=$srfi SCHEME=$scheme all install"
-                echo "  timeout 120 make SRFI=$srfi SCHEME=$scheme test"
+                echo "  timeout 600 make SRFI=$srfi SCHEME=$scheme test"
                 echo "}"
             } >> "tests.bats"
         done
